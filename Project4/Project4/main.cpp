2cf783a350444765cce22ea00a0694560f303391
paul.gabrielsson@gmail.com <paul.gabrielsson@gmail.com@613ea2d5-415c-ca99-c918-92f6dc05f32f>
2013-10-03 07:10:22 +0000

    Checked out on Oct 3rd 2013b @ ORU

---
+++ Project4/Project4/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <clocale>
+#include "Passning.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "swedish");
+
+	Instring input;
+	int i = 0;
+	string* argv[1];
+
+	cout << "Skriv hur många teckenrader som skall skrivas! Avsluta med radslut (retur)" << endl;
+	i = input.GetAntal();
+	cin.get();
+	cout << "Raden skall skrivas ut " << i << " gånger" << endl;
+	cout << "\nSedan skriv raden, använd godtyckliga tecken! Avsluta teckensträngen med radslut (retur)" << endl;
+	argv[0] = input.Return_Str_ptr();
+	cout << "\n";
+	cout << "Raderna skrivs ut här nedanför: " << endl;
+	input.Passning(i, argv);
+	cin.get();
+
+	return 0;
+}
